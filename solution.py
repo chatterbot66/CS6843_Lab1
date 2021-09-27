21dbb344fa2234c86002b7b7bbb3260daef95d94
chatterbot66 <70983339+chatterbot66@users.noreply.github.com>
2021-09-27 18:41:51 -0400

    first commit

---
+++ solution.py
@@ -0,0 +1,26 @@
+## Name John Calve; jnc330
+## Assignment 1: Q & A
+
+def welcome_assignment_answers(question):
+    # Not have to follow skeleton in assignment
+    # Can use "case" statements similar to C.
+    if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+        answer = "mTLS"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "is is a hashed message supposed to be unhashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        answer = "b21bed4510d4c731cf6a8afe2c2bc071"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = int(4)
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        return = int(5)
+
+
